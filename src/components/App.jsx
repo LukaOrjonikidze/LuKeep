e6a692339ba78b46f0d46cfa96ddee99c5c8d49b
LukaOrjonikidze <lukaorjonikidze24@gmail.com>
2023-02-09 17:56:01 +0400

    added localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    let localNotes = localStorage.getItem("notes");
+    if (localNotes){
+      localNotes = JSON.parse(localStorage.getItem("notes"));
+    } else {
+      localNotes = [];
+    }
+    setNotes(localNotes);
+  }, [])
+  useEffect( () => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
 
   function addNote(note) {
     setNotes((prevValue) => [...prevValue, note]);
